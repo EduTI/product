35bdfaaf755b094b7624bf9fdb7cde14dd29807a
Eduardo <eduuaardooooo93@gmail.com>
2019-04-12 22:21:07 -0300

    Exe. 23 - Configurações gerais.

--- src/main/java/br/com/etechoracio/product/ProductApplication.java
+++ src/main/java/br/com/etechoracio/product/ProductApplication.java
@@ -4,6 +4,7 @@ import javax.faces.webapp.FacesServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductApplication {
@@ -12,6 +13,7 @@ public class ProductApplication {
 		SpringApplication.run(ProductApplication.class, args);
 	}
 
+	@Bean
 	public ServletRegistrationBean<FacesServlet> servletRegistrationBean(){
 		return new ServletRegistrationBean<FacesServlet>(new FacesServlet(), "*.xhtml");
 	}
